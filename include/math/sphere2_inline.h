c8090373388fc68db5f95e736394195b70f134f5
irov <irov13@mail.ru>
2019-11-15 14:17:39 +0200

    fix sphere2_inline.h

--- include/math/sphere2_inline.h
+++ include/math/sphere2_inline.h
@@ -1,4 +1,18 @@
+#include "math/sphere2.h"
+
 namespace mt
 {
+    MT_FUNCTION_INLINE bool sphere2_intersect_sphere2( const mt::sphere2 & _sphere0, const mt::sphere2 & _sphere1 )
+    {
+        float distance = _sphere0.radius + _sphere1.radius;
 
+        float sqrlength = mt::sqrlength_v2_v2( _sphere0.position, _sphere1.position );
+
+        if( sqrlength > distance * distance )
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
